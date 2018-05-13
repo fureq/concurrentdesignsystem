76a0978f8713c970107f1426a44a412405301356
fureq <furmanszymon@gmail.com>
2018-05-13 22:36:24 +0200

    fix typo in main class name

--- src/main/java/com/concurrentdesignsystem/concurrentdesignsystem/ConcurrentDesignSystemApplication.java
+++ src/main/java/com/concurrentdesignsystem/concurrentdesignsystem/ConcurrentDesignSystemApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConcurrentdesignsystemApplication {
+public class ConcurrentDesignSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConcurrentdesignsystemApplication.class, args);
+		SpringApplication.run(ConcurrentDesignSystemApplication.class, args);
 	}
 }
