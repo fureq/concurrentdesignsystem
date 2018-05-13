e4b652843a3671e7d5292655c0d8c732773fc2b0
fureq <furmanszymon@gmail.com>
2018-05-13 22:35:11 +0200

    init springboot app

---
+++ src/main/java/com/concurrentdesignsystem/concurrentdesignsystem/ConcurrentdesignsystemApplication.java
@@ -0,0 +1,12 @@
+package com.concurrentdesignsystem.concurrentdesignsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentdesignsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentdesignsystemApplication.class, args);
+	}
+}
